7cb8d87e196e4c29a104812394212f6182dc1690
Nxllpointer <54677650+Nxllpointer@users.noreply.github.com>
2024-05-07 16:42:31 +0200

    Fix bad game FPS on kwin

--- modules/system/desktop/default.nix
+++ modules/system/desktop/default.nix
@@ -4,4 +4,11 @@
     displayManager.sddm.enable = true;
     desktopManager.plasma5.enable = true;
   };
+
+  # Useable FPS for games. Kwin is weird
+  environment.sessionVariables = {
+    KWIN_X11_REFRESH_RATE = "144000";
+    KWIN_X11_NO_SYNC_TO_VBLANK = "1";
+    KWIN_X11_FORCE_SOFTWARE_VSYNC = "1";
+  };
 }
