c474d75b55627eaa913feac76dea4ebece1ad7f3
Nxllpointer <54677650+Nxllpointer@users.noreply.github.com>
2024-05-10 14:07:36 +0200

    Add nix-your-shell

--- modules/system/shell.nix
+++ modules/system/shell.nix
@@ -11,6 +11,8 @@
           set -x STARSHIP_CONFIG ${pkgs.starship}/share/starship/presets/pastel-powerline.toml
           eval (${pkgs.starship}/bin/starship init fish)
         end
+
+        ${pkgs.nix-your-shell}/bin/nix-your-shell fish | source
       '';
     };
   };
