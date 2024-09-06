9f76910df0545e02b15ecd491d264d0971f82d55
Nxllpointer <54677650+Nxllpointer@users.noreply.github.com>
2024-09-06 19:07:03 +0200

    Expose custom packages

--- flake/default.nix
+++ flake/default.nix
@@ -7,10 +7,14 @@ let
     lib.genAttrs supportedSystems (system:
       (f {
         inherit system;
-        pkgs = import nixpkgs { inherit system; };
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import ./../pkgs inputs) ];
+        };
       }));
 in
 {
   nixosConfigurations = (import ./nixosConfigurations.nix) lib inputs;
+  packages = forSupportedSystems (import ./packages.nix);
   formatter = forSupportedSystems (import ./formatter.nix);
 }
