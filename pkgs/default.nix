6dbfff0343403c20b99fc8fec5b35a1aa65f6c0f
Nxllpointer <54677650+Nxllpointer@users.noreply.github.com>
2024-12-03 14:09:48 +0100

    Use stable opentabletdriver

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -3,5 +3,4 @@ final: prev: {
   rhythia = final.callPackage ./rhythia {
     inherit (inputs) rhythia-git;
   };
-  opentabletdriver = inputs.opentabletdriver.packages.${prev.system}.opentabletdriver;
 }
