ddbbb150038d6d3e96aa5b88b76ceca67713cfad
Nxllpointer <54677650+Nxllpointer@users.noreply.github.com>
2025-01-04 21:21:48 +0100

    Update cpu microcode

--- configurations/home/default.nix
+++ configurations/home/default.nix
@@ -1,8 +1,10 @@
-{ config, lib, pkgs, ... }: {
+{
   imports = [ ./hardware-configuration.nix ./nvidia.nix ];
 
   boot.tmp.useTmpfs = true;
   boot.binfmt.emulatedSystems = [ "aarch64-linux" ];
+
+  hardware.cpu.amd.updateMicrocode = true;
 
   hardware.opentabletdriver = {
     enable = true;
