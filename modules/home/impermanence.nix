023b6ee850fdd6f99556df2932a099813f4f7b87
Nxllpointer <54677650+Nxllpointer@users.noreply.github.com>
2026-03-15 12:18:33 +0100

    Make persisted home dirs have orange icon

--- modules/home/impermanence.nix
+++ modules/home/impermanence.nix
@@ -1,12 +1,29 @@
+let
+  persistedDirs = [
+    "Documents"
+    "Downloads"
+    "Pictures"
+    "Videos"
+    "Music"
+    ".ssh"
+  ];
+in
 {
   home.persistence."/persisted" = {
-    directories = [
-      "Documents"
-      "Downloads"
-      "Pictures"
-      "Videos"
-      "Music"
-      ".ssh"
-    ];
+    directories = persistedDirs;
   };
+
+  home.file = builtins.listToAttrs (
+    map
+      (dir: {
+        name = "${dir}/.directory";
+        value = {
+          text = ''
+            [Desktop Entry]
+            Icon=folder-orange
+          '';
+        };
+      })
+      persistedDirs
+  );
 }
