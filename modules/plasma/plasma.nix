e7462a71537bd4d64a7cc84f60fbca2d261f312f
Nxllpointer <54677650+Nxllpointer@users.noreply.github.com>
2026-03-31 17:17:52 +0200

    Create kzones shortcuts

--- modules/plasma/plasma.nix
+++ modules/plasma/plasma.nix
@@ -1,4 +1,15 @@
-{
+let
+  # KZones: Move active window to zone 1=Num+1,none,KZones: Move active window to zone 1
+  kzonesMoveName = zone: "KZones: Move active window to zone ${zone}";
+  kzonesKwinShortcuts = builtins.listToAttrs (
+    map
+    (zone: {
+      name = kzonesMoveName zone;
+      value = "Num+${zone},none,${kzonesMoveName zone}";
+    })
+    ["1" "2" "3" "4" "5" "6" "7" "8" "9"]
+  );
+in {
   flake.modules.nixos.base = {pkgs, ...}: {
     services = {
       displayManager.sddm = {
@@ -13,9 +24,14 @@
 
   flake.modules.homeManager.gui = {pkgs, ...}: {
     home.packages = [pkgs.kdePackages.kzones];
+
     qt.kde.settings.kwinrc = {
       Script-kzones.layoutsJson = builtins.readFile ./kzones-layouts.json;
       Plugins.kzonesEnabled = true;
+    };
+
+    qt.kde.settings.kglobalshortcutsrc = {
+      kwin = kzonesKwinShortcuts;
     };
   };
 
