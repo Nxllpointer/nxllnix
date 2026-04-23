d89490aaf4261b7c969fc2460d7254eabe7864e3
Nxllpointer <54677650+Nxllpointer@users.noreply.github.com>
2026-04-23 16:33:26 +0200

    Add konsole launch shortcut

--- modules/plasma/plasma.nix
+++ modules/plasma/plasma.nix
@@ -11,6 +11,13 @@
     };
   };
 
+  flake.modules.homeManager.gui = {
+    qt.kde.settings.kglobalshortcutsrc = {
+      services."org.kde.konsole.desktop"._launch = "Meta+T";
+      kwin."Edit Tiles" = "none,Meta+T,Toggle Tiles Editor";
+    };
+  };
+
   flake.modules.homeManager.impermanence = {
     persisted = {
       files = [
