8efb685aae460d606aa4d7fc28d30da9141a39c0
Nxllpointer <54677650+Nxllpointer@users.noreply.github.com>
2026-04-23 17:09:18 +0200

    Make KDE animations instant

--- modules/plasma/plasma.nix
+++ modules/plasma/plasma.nix
@@ -12,11 +12,17 @@
   };
 
   flake.modules.homeManager.gui = {
-    qt.kde.settings.kglobalshortcutsrc = {
-      services."org.kde.konsole.desktop"._launch = "Meta+T";
-      kwin."Edit Tiles" = "none,Meta+T,Toggle Tiles Editor";
+    qt.kde.settings = {
+      kglobalshortcutsrc = {
+        services."org.kde.konsole.desktop"._launch = "Meta+T";
+        kwin."Edit Tiles" = "none,Meta+T,Toggle Tiles Editor";
 
-      services."firefox.desktop"._launch = "Meta+F";
+        services."firefox.desktop"._launch = "Meta+F";
+      };
+
+      kdeglobals = {
+        KDE.AnimationDurationFactor = 0; # Instant animations
+      };
     };
   };
 
