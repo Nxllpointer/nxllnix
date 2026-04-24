56c2b25669ced69a6ba0a291440c7c47a1af14e5
Nxllpointer <54677650+Nxllpointer@users.noreply.github.com>
2026-04-24 16:13:31 +0200

    Add 5 virtual desktops

--- modules/plasma/plasma.nix
+++ modules/plasma/plasma.nix
@@ -27,6 +27,18 @@
       kdeglobals = {
         KDE.AnimationDurationFactor = 0; # Instant animations
       };
+
+      kwinrc = {
+        Desktops = {
+          Id_1 = "d40ae7d8-76a1-47ce-84f6-73ca391f3e5e";
+          Id_2 = "e5134201-07bc-4f78-b68e-800f44bcd04d";
+          Id_3 = "c9f53370-9829-4193-876e-9eeca5f17f82";
+          Id_4 = "337f3589-e9b5-4454-a08a-21bb710fdd3c";
+          Id_5 = "19dec7d0-138a-4468-84c5-52c64e4b500f";
+          Number = 5;
+          Rows = 1;
+        };
+      };
     };
 
     home.activation.plasmaTheme = lib.hm.dag.entryAfter ["writeBoundary"] ''
