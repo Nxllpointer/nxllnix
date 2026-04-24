c0556082056888c083e48ea469a37057793c3624
Nxllpointer <54677650+Nxllpointer@users.noreply.github.com>
2026-04-24 18:25:50 +0200

    Declare plasma widget configuration

--- modules/plasma/plasma.nix
+++ modules/plasma/plasma.nix
@@ -38,6 +38,62 @@
           Number = 5;
           Rows = 1;
         };
+
+        Effect-overview.BorderActivate = "";
+      };
+
+      kactivitymanagerdrc = {
+        activities = {
+          b5640d09-faba-4fa5-a878-9379272d649a = "Default";
+        };
+      };
+
+      "plasma-org.kde.plasma.desktop-appletsrc" = {
+        Containments."1" = rec {
+          activityId = "b5640d09-faba-4fa5-a878-9379272d649a";
+          formfactor = 0;
+          immutability = 1;
+          lastScreen = 0;
+          location = 0;
+          plugin = "org.kde.plasma.folder";
+          wallpaperplugin = "org.kde.image";
+          ItemGeometriesHorizontal = "Applet-11101:0,0,256,480,0;Applet-11102:256,0,256,208,0;Applet-11103:256,208,256,272,0;";
+          ItemGeometries-3840x2160 = ItemGeometriesHorizontal;
+
+          Applets = {
+            "11101".plugin = "org.kde.plasma.systemmonitor.cpucore";
+            "11102".plugin = "org.kde.plasma.systemmonitor.memory";
+            "11103".plugin = "org.kde.plasma.systemmonitor.diskusage";
+          };
+        };
+
+        Containments."2" = {
+          activityId = "";
+          formfactor = 2;
+          immutability = 1;
+          lastScreen = 0;
+          location = 4;
+          plugin = "org.kde.panel";
+
+          Applets = {
+            "22201".plugin = "org.kde.plasma.kickoff";
+            "22202".plugin = "org.kde.plasma.pager";
+            "22203".plugin = "org.kde.plasma.icontasks";
+            "22204".plugin = "org.kde.plasma.marginsseparator";
+            "22205".plugin = "org.kde.plasma.systemtray";
+            "22206" = {
+              plugin = "org.kde.plasma.digitalclock";
+              Configuration.Appearance.use24hFormat = 2;
+            };
+            "22207".plugin = "org.kde.plasma.showdesktop";
+          };
+
+          General.AppletOrder = "22201;22202;22203;22204;22205;22206;22207";
+        };
+      };
+
+      plasmashellrc = {
+        PlasmaViews."Panel 2".Defaults.thickness = 46;
       };
 
       konsolerc = {
