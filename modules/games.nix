d07f463386f1eb769694ec60eb3cd7ec0ba2c249
Nxllpointer <54677650+Nxllpointer@users.noreply.github.com>
2026-08-18 11:34:18 +0200

    Osu!!

--- modules/games.nix
+++ modules/games.nix
@@ -5,14 +5,16 @@
     ...
   }: {
     home = {pkgs, ...}: {
-      home.packages = [
-        config.flake.packages.${pkgs.stdenv.hostPlatform.system}.rhythia
+      home.packages = with pkgs; [
+        config.flake.packages.${stdenv.hostPlatform.system}.rhythia
+        pkgs.osu-lazer-bin
       ];
 
       programs.prismlauncher.enable = true;
 
       persisted.directories = lib.mkIf globalconfig.impermanence.enable [
         ".local/share/SoundSpacePlus"
+        ".local/share/osu"
         ".local/share/PrismLauncher"
       ];
     };
