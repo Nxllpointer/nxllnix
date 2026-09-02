a4896861cd65a62515008ad900ac95ec8ef3fe0c
Nxllpointer <54677650+Nxllpointer@users.noreply.github.com>
2026-09-02 16:47:28 +0200

    Move corecycler to performance module

---
+++ modules/configurations/orion/performance.nix
@@ -0,0 +1,30 @@
+{inputs, ...}: {
+  configurations.nxllnix-orion = {
+    nixos = {pkgs, ...}: {
+      imports = [
+        inputs.corecycler.nixosModules.default
+      ];
+
+      services.corecycler = {
+        enable = true;
+        deviceAccessUser = "nxll";
+      };
+
+      environment.systemPackages = [
+        (pkgs.mprime.overrideAttrs (prev: {
+          src = prev.src.overrideAttrs (prevSrc: {
+            urls = [
+              (
+                # .ca redirects to google drive and shows a popup
+                builtins.replaceStrings
+                ["https://download.mersenne.ca/gimps/"]
+                ["https://www.mersenne.org/download/software/"]
+                prev.src.url
+              )
+            ];
+          });
+        }))
+      ];
+    };
+  };
+}
